b594f7f595e34c51fca5b8b8509c2835e79356ad
dukerupert <logan@fireflysoftware.dev>
2025-03-04 01:54:45 +0000

    Project reset. Added logging.

--- main.go
+++ main.go
@@ -1,27 +1,75 @@
 package main
 
 import (
+	"flag"
+	"io"
+	"net/http"
+	"os"
+	"text/template"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"net/http"
+	"github.com/rs/zerolog"
 )
 
-// Record represents a vinyl record in the collection
-type Record struct {
-	ID        string `json:"id"`
-	Artist    string `json:"artist"`
-	Album     string `json:"album"`
-	Year      int    `json:"year"`
-	Genre     string `json:"genre"`
-	Condition string `json:"condition"`
-}
-
 func main() {
+	// UNIX Time is faster and smaller than most timestamps
+	debug := flag.Bool("debug", false, "sets log level to debug")
+	flag.Parse()
+
+	// Configure global settings
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	// Create the main logger
+	var logger zerolog.Logger
+	if *debug {
+		// Pretty console logging in debug mode
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+		consoleWriter := zerolog.ConsoleWriter{Out: os.Stderr}
+		logger = zerolog.New(consoleWriter).With().Timestamp().Logger()
+	} else {
+		// JSON logging for production
+		logger = zerolog.New(os.Stderr).With().Timestamp().Logger()
+	}
+
+	// Parse template files
+	t := &Template{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
 	// Create a new Echo instance
 	e := echo.New()
+	e.Renderer = t
 
 	// Middleware
-	e.Use(middleware.Logger())
+	headers := []string{echo.HeaderAuthorization}
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogURI:     true,
+		LogStatus:  true,
+		LogMethod:  true,
+		LogHeaders: headers,
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			// Create a map to store header values
+			headerValues := make(map[string]string)
+
+			// Extract each header value from the request
+			for _, header := range headers {
+				headerValues[header] = c.Request().Header.Get(header)
+			}
+			logger.Info().
+				Str("Method", v.Method).
+				Str("URI", v.URI).
+				Int("Status", v.Status).
+				Fields(map[string]interface{}{
+					"headers": headers,
+				}).Fields(map[string]interface{}{
+				"headers": headerValues,
+			}).
+				Msg("request")
+
+			return nil
+		},
+	}))
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORS())
 
@@ -29,70 +77,25 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Welcome to Vinyl Collection API")
 	})
+	e.GET("/hello", Hello)
 
 	// Record endpoints
-	e.GET("/records", getAllRecords)
-	e.GET("/records/:id", getRecord)
-	e.POST("/records", createRecord)
-	e.PUT("/records/:id", updateRecord)
-	e.DELETE("/records/:id", deleteRecord)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
-// Handler functions
-func getAllRecords(c echo.Context) error {
-	// TODO: Implement database integration
-	records := []Record{
-		{
-			ID:        "1",
-			Artist:    "Pink Floyd",
-			Album:     "Dark Side of the Moon",
-			Year:      1973,
-			Genre:     "Progressive Rock",
-			Condition: "Very Good",
-		},
-	}
-	return c.JSON(http.StatusOK, records)
+// Handlers
+
+type Template struct {
+	templates *template.Template
 }
 
-func getRecord(c echo.Context) error {
-	id := c.Param("id")
-	// TODO: Implement database lookup
-	record := Record{
-		ID:        id,
-		Artist:    "Pink Floyd",
-		Album:     "Dark Side of the Moon",
-		Year:      1973,
-		Genre:     "Progressive Rock",
-		Condition: "Very Good",
-	}
-	return c.JSON(http.StatusOK, record)
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
 }
 
-func createRecord(c echo.Context) error {
-	record := new(Record)
-	if err := c.Bind(record); err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-	}
-	// TODO: Implement database creation
-	return c.JSON(http.StatusCreated, record)
-}
-
-func updateRecord(c echo.Context) error {
-	id := c.Param("id")
-	record := new(Record)
-	if err := c.Bind(record); err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-	}
-	record.ID = id
-	// TODO: Implement database update
-	return c.JSON(http.StatusOK, record)
-}
-
-func deleteRecord(c echo.Context) error {
-	// id := c.Param("id")
-	// TODO: Implement database deletion
-	return c.NoContent(http.StatusNoContent)
+func Hello(c echo.Context) error {
+	c.Response().Header().Set(echo.HeaderAcceptEncoding, "application/json")
+	return c.Render(http.StatusOK, "hello", "World")
 }
