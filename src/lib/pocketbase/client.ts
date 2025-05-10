ed64b29572251ee3d2cac6b5b6fa75d38ebb07af
dukerupert <logan@fireflysoftware.dev>
2025-05-10 08:42:51 -0600

    Rest

--- src/lib/pocketbase/client.ts
+++
@@ -1,8 +0,0 @@
-import PocketBase from 'pocketbase';
-import { PUBLIC_POCKETBASE_URL } from '$env/static/public'
-
-const pb_url = PUBLIC_POCKETBASE_URL || 'http://127.0.0.1:8090'
-
-const pb = new PocketBase(pb_url);
-
-export default pb
--- src/routes/dashboard/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import type { PageProps } from './$types'
-
-    let { data }: PageProps = $props();
-    $inspect(data)
-</script>
-
-<h1>Albums</h1>
